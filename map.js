eae47e0f608e77bc8660c3d1760c9cd0da72a477
Alfred <74591252+choiocampo@users.noreply.github.com>
2023-04-15 11:21:56 +0800

    Update map.js
    If coordinates are present in the URL, use them as the center, else use the default center. Added markers at the end of the geometry lines and on the streetview.

--- map.js
+++ map.js
@@ -1,12 +1,25 @@
+
 function initMap() {
 
-    const mapOptions = {
-        center: { lat: 42.33083643055002, lng: -83.03585974300418 }, // Detroit
-        zoom: 19,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        scrollwheel: true, // Enable mouse scroll wheel
+    function getUrlParams() {
+        const urlParams = new URLSearchParams(window.location.search);
+        const lat = parseFloat(urlParams.get('lat'));
+        const lng = parseFloat(urlParams.get('lng'));
 
+        return { lat, lng };
     }
+
+    const urlParams = getUrlParams();
+    const defaultCenter = { lat: 42.33083643055002, lng: -83.03585974300418 }; // Default center coordinates
+
+
+    const mapOptions = {
+        // If coordinates are present in the URL, use them as the center, else use the default center
+        center: urlParams.lat && urlParams.lng ? { lat: urlParams.lat, lng: urlParams.lng } : defaultCenter,
+        zoom: 19,
+    };
+
+
     const mainMap = new google.maps.Map(document.getElementById("main-map"), mapOptions);
     mainMap.data.addGeoJson(yourGeoJsonData);
 
@@ -18,11 +31,12 @@ function initMap() {
     // MOSTLY DITO NAGLALAGAY NG ADDITIONAL FEATURES
 
 
+
     // Set the style for the GeoJSON geometry
     mainMap.data.setStyle({
         fillColor: 'blue',
         strokeColor: 'blue',
-        strokeWeight: 2
+        strokeWeight: 3
     });
 
     // Create an info window
@@ -100,28 +114,99 @@ function initMap() {
         toggleView(mainMap);
     });
 
-    function includeHTML(file, elementId) {
-        const xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4) {
-                if (this.status == 200) {
-                    document.getElementById(elementId).innerHTML = this.responseText;
-                }
-                if (this.status == 404) {
-                    document.getElementById(elementId).innerHTML = "Page not found.";
-                }
-            }
+
+    // Function to add markers at the start and end points of the line on both maps
+    function addMarkers(start, end) {
+        const startIconUrl = "http://maps.google.com/mapfiles/kml/paddle/blu-circle.png";
+        const endIconUrl = "http://maps.google.com/mapfiles/kml/paddle/stop.png";
+
+        const startMarkerMain = new google.maps.Marker({
+            position: start,
+            map: mainMap,
+            title: "Start",
+            icon: startIconUrl
+        });
+
+        const endMarkerMain = new google.maps.Marker({
+            position: end,
+            map: mainMap,
+            title: "End",
+            icon: endIconUrl
+        });
+
+        const startMarkerStreet = new google.maps.Marker({
+            position: start,
+            map: streetView,
+            title: "Start",
+            icon: startIconUrl
+        });
+
+        const endMarkerStreet = new google.maps.Marker({
+            position: end,
+            map: streetView,
+            title: "End",
+            icon: endIconUrl
+        });
+
+        return {
+            main: [startMarkerMain, endMarkerMain],
+            street: [startMarkerStreet, endMarkerStreet]
         };
-        xhttp.open("GET", file, true);
-        xhttp.send();
     }
 
-    window.addEventListener("load", () => {
-        initMap();
-        includeHTML("window.html", "empty-window");
+    // Add a click event listener to the mainMap.data object
+    mainMap.data.addListener("click", (event) => {
+        // ... (existing code)
+
+        // Display the line on streetView
+        const path = event.feature.getGeometry().getArray();
+        const polyline = new google.maps.Polyline({
+            path: path,
+            geodesic: true,
+            strokeColor: "#FF0000",
+            strokeOpacity: 1.0,
+            strokeWeight: 2
+        });
+
+        polyline.setMap(streetView);
+
+        // Add markers at the start and end points of the line on both maps
+        const { main: [startMarkerMain, endMarkerMain], street: [startMarkerStreet, endMarkerStreet] } = addMarkers(path[0], path[path.length - 1]);
+
+        // Remove markers and polyline when the info window is closed
+        google.maps.event.addListener(infoWindow, "closeclick", () => {
+            startMarkerMain.setMap(null);
+            endMarkerMain.setMap(null);
+            startMarkerStreet.setMap(null);
+            endMarkerStreet.setMap(null);
+            polyline.setMap(null);
+        });
     });
+
+
 
 }
 
+
 google.maps.event.addDomListener(window, "load", initMap);
 window.addEventListener("load", initMap);
+function includeHTML(file, elementId) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                document.getElementById(elementId).innerHTML = this.responseText;
+            }
+            if (this.status == 404) {
+                document.getElementById(elementId).innerHTML = "Page not found.";
+            }
+        }
+    };
+    xhttp.open("GET", file, true);
+    xhttp.send();
+}
+
+window.addEventListener("load", () => {
+    initMap();
+    includeHTML("window.html", "empty-window");
+});
